5d430aba123987eb8eb34a222ad4a64c0272f282
RP <rogerpino94@gmail.com>
2021-08-09 08:46:22 -0500

    Select para cupones agregado

--- src/pricingAndDiscounts/pricingAndDiscounts.js
+++ src/pricingAndDiscounts/pricingAndDiscounts.js
@@ -1,7 +1,29 @@
 function calcularDescuento(
   price = parseInt(document.getElementById("inputPrice").value),
-  discount = parseInt(document.getElementById("inputDiscount").value)
+  discount = parseInt(document.getElementById("inputDiscount").value),
+  coupon = document.getElementById("selectCoupon").selectedIndex
 ) {
-  let finalPrice = (price * (100 - discount)) / 100;
-  document.getElementById("lblFinalPrice").innerHTML = finalPrice;
+  if (isNaN(price)) {
+    alert("Ingrese el precio a calcular");
+  } else {
+    switch (coupon) {
+      case 1:
+        discount = discount + 15;
+        break;
+      case 2:
+        discount = discount + 25;
+        break;
+      case 3:
+        discount = discount + 50;
+        break;
+      case 4:
+        discount = discount + 60;
+        break;
+      default:
+        discount = discount;
+    }
+
+    let finalPrice = (price * (100 - discount)) / 100;
+    document.getElementById("lblFinalPrice").innerHTML = `$${finalPrice}`;
+  }
 }
